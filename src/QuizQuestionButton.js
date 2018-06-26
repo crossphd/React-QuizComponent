03444b5529876f31dfca52f6d1ac830226964860
Christopher Ross <cross25@uis.edu>
2018-06-26 14:33:51 -0700

    committing section 4

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class QuizQuestionButton extends Component {
+
+  render() {
+    return(
+      <li>
+        <button>{this.props.buttonText}</button>
+      </li>
+    )
+  }
+}
+
+export default QuizQuestionButton;
