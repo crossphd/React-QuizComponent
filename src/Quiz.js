bb3e81ecc63eacc19af1fd78820fbfecf655cf54
Christopher Ross <cross25@uis.edu>
2018-06-26 13:18:40 -0700

    fixed issue in first submission

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,10 +6,17 @@ class Quiz extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      quiz_position: 1
+    }
   }
 
-  Render() {
-    <div>Quiz</div>
+  render() {
+    return (
+      <div>
+        <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+      </div>
+    )
   }
 }
 
